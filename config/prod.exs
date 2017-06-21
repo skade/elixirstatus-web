62c5ac32a955283457d84805764afd20656a32d6
Florian Gilcher <florian.gilcher@asquera.de>
2017-06-21 09:39:06 +0000

    Fix prod config

--- config/prod.exs
+++ config/prod.exs
@@ -25,10 +25,10 @@ config :elixir_status, ElixirStatus.Repo,
   ssl: true
 
 config :ex_twitter, :oauth, [
-   consumer_key: String.to_integer(System.get_env("TWITTER_CONSUMER_KEY")),
-   consumer_secret: String.to_integer(System.get_env("TWITTER_CONSUMER_SECRET")),
-   access_token: String.to_integer(System.get_env("TWITTER_ACCESS_TOKEN")),
-   access_token_secret: String.to_integer(System.get_env("TWITTER_ACCESS_SECRET"))
+   consumer_key: System.get_env("TWITTER_CONSUMER_KEY"),
+   consumer_secret: System.get_env("TWITTER_CONSUMER_SECRET"),
+   access_token: System.get_env("TWITTER_ACCESS_TOKEN"),
+   access_token_secret: System.get_env("TWITTER_ACCESS_SECRET")
 ]
 
 # ## SSL Support
